0967d65226c854317146e23615bc6edd919b1a05
Maxim Yefremov <maxYefr@gmail.com>
2019-08-21 11:17:37 +0300

    removed glog dependancy

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package jsn
 
 import (
 	"encoding/json"
-
-	"github.com/golang/glog"
 )
 
 func B(inp interface{}) string {
 	b, err := json.MarshalIndent(&inp, "", "   ")
 	if err != nil {
-		glog.Error(err)
 		return err.Error()
 	}
 	return string(b)
